c2e686cf29b7c4196d71d55aa3118a4a76276941
prawin <yadavprawin126@gmail.com>
2025-01-17 19:26:39 +0545

    project well and running

--- Connectify/tweet/views.py
+++ Connectify/tweet/views.py
@@ -8,33 +8,33 @@ def index(request):
     return render(request, 'index.html')
 
 def tweet_list(request):
-    tweets = Tweet.objects.all().order_by('-created_at')
+    tweets = Tweet.objects.all().order_by('-create_at')
     return render(request, 'tweet_list.html', {'tweets': tweets})
 
 def create_tweet(request):
     if request.method == 'POST':
-        form = TweetFrom(request.POST, request.FILES)
+        form = TweetForm(request.POST, request.FILES)
         if form.is_valid():
             tweet = form.save(commit=False)
             tweet.user = request.user
             tweet.save()
             return redirect('tweet_list')
     else:
-        form = TweetFrom()
+        form = TweetForm()
     return render(request, 'tweet_form.html', {'form': form})
 
 
 def edit_tweet(request,tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user = request.user)
     if request.method == 'POST':
-        form = TweetFrom(request.POST, request.FILES, instance=tweet)
+        form = TweetForm(request.POST, request.FILES, instance=tweet)
         if form.is_valid():
             tweet = form.save(commit=False)
             tweet.user = request.user
             tweet.save()
             return redirect('tweet_list')
     else:
-        form = TweetFrom(instance=tweet)
+        form = TweetForm(instance=tweet)
     return render(request, 'tweet_form.html', {'form': form})
 
 
